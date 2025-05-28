5de8b95694bfb78dc8d7d11958d08c7ad62200ee
marco <marcotaku@gmail.com>
2025-05-28 19:24:45 -0400

    2do commit

--- src/main/java/cl/marco/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/cl/marco/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_API_KEY"));
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-4.1")
